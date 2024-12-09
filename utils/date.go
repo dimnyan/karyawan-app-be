9c3c6d2d3c4c71d11b2259383c95cef710bedca1
Nyoman Lanang <dimasnyoman1@gmail.com>
2024-12-09 12:07:29 +0700

    add date time util

--- utils/date.go
+++ utils/date.go
@@ -5,7 +5,17 @@ import "time"
 func ParseDate(dateParam string) time.Time {
 	date, err := time.Parse("2006-01-02", dateParam)
 	if err != nil {
-		ErrorResponse(err)
+		ErrorMessage("Cannot parse date")
+		return time.Time{}
 	}
 	return date
+}
+
+func ParseDateTime(dateTimeParam string) int64 {
+	dateTime, err := time.Parse("2006-01-02 15:04 MST", dateTimeParam)
+	if err != nil {
+		ErrorMessage("Cannot parse date time")
+		return 0
+	}
+	return dateTime.Unix()
 }
