ce146a4b9181f9a61835cd7449661f704fa3eb7e
Nyoman Lanang <dimasnyoman1@gmail.com>
2024-12-09 12:07:15 +0700

    add check token

--- api/auth.go
+++ api/auth.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"karyawan-app-be/utils"
@@ -56,11 +57,63 @@ func (server *Server) Login(ctx *gin.Context) {
 	}
 	// Token : Set Cookie
 	http.SetCookie(ctx.Writer, &http.Cookie{
-		Name:  "token",
-		Value: stringToken,
+		Name:     "token",
+		Value:    stringToken,
+		HttpOnly: true,
 	})
 	// Token: Give Response
 	var loginResponse LoginResponse
 	loginResponse.Token = stringToken
 	ctx.JSON(http.StatusOK, loginResponse)
+}
+
+type TokenRequest struct {
+	Token string `json:"token"`
+}
+
+func (server *Server) CheckToken(ctx *gin.Context) {
+	tokenString, err := ctx.Cookie("token")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, utils.ErrorMessage("Invalid Token on Cookie"))
+		ctx.Abort()
+		return
+	}
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret-key"), nil
+	})
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, utils.ErrorResponse(err))
+		ctx.Abort()
+		return
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		ctx.JSON(http.StatusUnauthorized, utils.ErrorMessage("Unauthorized Token"))
+	}
+
+	// Extract role (assuming it's a simple integer)
+	roleValue, ok := claims["role"].(map[string]interface{})
+	if ok {
+		roleInt, ok := roleValue["Int64"].(float64)
+		if ok {
+			fmt.Println("Role:", int(roleInt))
+		} else {
+			fmt.Println("Error extracting role")
+		}
+	} else {
+		fmt.Println("Error extracting role")
+	}
+
+}
+
+func (server *Server) Logout(ctx *gin.Context) {
+	http.SetCookie(ctx.Writer, &http.Cookie{
+		Name:     "token",
+		Value:    "",
+		Expires:  time.Now().AddDate(-1, 0, 0),
+		HttpOnly: true,
+	})
 }
